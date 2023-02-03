42b37ef2c5e5a49da64715ee814b4d7797ff1faa
Mike <MikePetrus87@gmail.com>
2023-02-02 16:22:20 -0800

    Added mode select to define UI changes on button press

--- app/src/main/java/com/petrus/todo/ListLoader.java
+++ app/src/main/java/com/petrus/todo/ListLoader.java
@@ -12,13 +12,14 @@ import java.util.ArrayList;
 
 public class ListLoader
 {
-    public static final String FILENAME = "list_items.dat";
+    public static final String TODOFILE = "list_items.dat";
+    public static final String GROCERYFILE = "grocery_items.dat";
 
-    public static void writeData (ArrayList<Task> toDo, Context context)
+    public static void writeToDo (ArrayList<Task> toDo, Context context)
     {
         try
         {
-            FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            FileOutputStream fos = context.openFileOutput(TODOFILE, Context.MODE_PRIVATE);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(toDo);
             oos.close();
@@ -28,13 +29,51 @@ public class ListLoader
         }
     }
 
-    public static ArrayList<Task> readData (Context context)
+    public static ArrayList<Task> readToDo (Context context)
     {
         ArrayList<Task> toDo = null;
 
         try
         {
-            FileInputStream fis = context.openFileInput(FILENAME);
+            FileInputStream fis = context.openFileInput(TODOFILE);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            toDo = (ArrayList<Task>) ois.readObject();
+            ois.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            toDo = new ArrayList<>();
+            e.printStackTrace();
+        }
+        catch (IOException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        return toDo;
+    }
+
+    public static void writeToGrocery (ArrayList<Task> toDo, Context context)
+    {
+        try
+        {
+            FileOutputStream fos = context.openFileOutput(GROCERYFILE, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(toDo);
+            oos.close();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Task> readGrocery (Context context)
+    {
+        ArrayList<Task> toDo = null;
+
+        try
+        {
+            FileInputStream fis = context.openFileInput(GROCERYFILE);
             ObjectInputStream ois = new ObjectInputStream(fis);
             toDo = (ArrayList<Task>) ois.readObject();
             ois.close();
--- app/src/main/java/com/petrus/todo/MainActivity.java
+++ app/src/main/java/com/petrus/todo/MainActivity.java
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements RecyclerViewInterface
 {
     EditText editTextAddItem;
+    TextView textViewTitle;
     Button buttonAdd;
+    MaterialButton buttonToDo, buttonGrocery;
     RecyclerView recyclerViewToDo;
     ArrayList<Task> arrayToDo = new ArrayList<>();
+    ArrayList<Task> arrayGroceries = new ArrayList<>();
     RecyclerViewAdapter adapter;
+    SharedPreferences sharedPreferences;
+    String mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,23 +35,43 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = this.getSharedPreferences("Preferences", MODE_PRIVATE);
+        mode = sharedPreferences.getString("mode", "toDo");
+
         editTextAddItem = findViewById(R.id.editTextAddItem);
+        textViewTitle = findViewById(R.id.textViewTitle);
         buttonAdd = findViewById(R.id.buttonAdd);
+        buttonToDo = findViewById(R.id.buttonToDo);
+        buttonGrocery = findViewById(R.id.buttonGrocery);
         recyclerViewToDo = findViewById(R.id.recyclerViewToDo);
 
-        arrayToDo = ListLoader.readData(this);
+        arrayToDo = ListLoader.readToDo(this);
+        arrayGroceries = ListLoader.readGrocery(this);
+
         adapter = new RecyclerViewAdapter(arrayToDo, this, this);
 
         recyclerViewToDo.setAdapter(adapter);
         recyclerViewToDo.setLayoutManager(new LinearLayoutManager(this));
+
+        modeSelect(mode);
 
         buttonAdd.setOnClickListener(view ->
         {
             Task newItem = new Task(editTextAddItem.getText().toString());
             arrayToDo.add(newItem);
             editTextAddItem.setText(null);
-            ListLoader.writeData(arrayToDo, getApplicationContext());
+            ListLoader.writeToDo(arrayToDo, getApplicationContext());
             adapter.notifyDataSetChanged();
+        });
+
+        buttonToDo.setOnClickListener(view ->
+        {
+            modeSelect("toDo");
+        });
+
+        buttonGrocery.setOnClickListener(view ->
+        {
+            modeSelect("grocery");
         });
     }
 
@@ -57,7 +86,27 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         {
             arrayToDo.get(position).setComplete(false);
         }
-        ListLoader.writeData(arrayToDo, getApplicationContext());
+        ListLoader.writeToDo(arrayToDo, getApplicationContext());
         adapter.notifyDataSetChanged();
+    }
+
+    public void modeSelect(String mode)
+    {
+        switch(mode)
+        {
+            case "toDo":
+                textViewTitle.setText(R.string.app_name);
+                buttonToDo.setIconTintResource(R.color.white);
+                buttonGrocery.setIconTintResource(R.color.gray);
+                recyclerViewToDo.setBackgroundColor(getColor(R.color.light_gray));
+                break;
+
+            case "grocery":
+                textViewTitle.setText(R.string.groceries);
+                buttonGrocery.setIconTintResource(R.color.white);
+                buttonToDo.setIconTintResource(R.color.gray);
+                recyclerViewToDo.setBackgroundColor(getColor(R.color.light_blue));
+                break;
+        }
     }
 }
